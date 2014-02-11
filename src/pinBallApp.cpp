88a2df6e1543357818cac4cb0491cf5f5ee841f2
Wouter <wouter.vandersyppe@gmail.com>
2014-02-11 12:49:28 +0100

    refractored testapp classname

---
+++ src/pinBallApp.cpp
@@ -0,0 +1,63 @@
+#include "pinbBallApp.h"
+
+//--------------------------------------------------------------
+void pinBall::setup(){
+    ofEnableSmoothing();
+    ofSetBackgroundColor(33);
+    
+}
+
+//--------------------------------------------------------------
+void pinBall::update(){
+
+}
+
+//--------------------------------------------------------------
+void pinBall::draw(){
+
+}
+
+//--------------------------------------------------------------
+void pinBall::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void pinBall::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void pinBall::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void pinBall::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void pinBall::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void pinBall::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void pinBall::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void pinBall::gotMessage(ofMessage msg){
+    ofLogWarning() << "Got Message: " << msg.message;
+}
+
+//--------------------------------------------------------------
+void pinBall::dragEvent(ofDragInfo dragInfo){ 
+
+}
